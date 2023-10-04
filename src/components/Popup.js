8f700cbd7ca91c881601d9b8ec39c56ecb69d6da
Home <home@gmail.com>
2023-10-04 21:08:50 +0300

    fix: resets values to popup when closing without saving

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,6 +1,8 @@
 export default class Popup {
   constructor(popupSelector) {
     this._popup = document.querySelector(popupSelector);
+    this.close = this.close.bind(this);
+    this.open = this.open.bind(this);
   }
 
   _handleEscClose = ({ key }) => {
@@ -17,13 +19,13 @@ export default class Popup {
     })
   };
 
-  close = () => {
+  close() {
     this._popup.classList.remove('popup_shown');
 
     document.removeEventListener('keydown', this._handleEscClose);
   };
 
-  open = () => {
+  open() {
     this._popup.classList.add('popup_shown');
 
     document.addEventListener('keydown', this._handleEscClose);
--- src/components/PopupWithChooseAddress.js
+++ src/components/PopupWithChooseAddress.js
@@ -12,11 +12,25 @@ export default class PopupWithChooseAddress extends Popup {
     this._popupPickupPointTab = document.querySelector('.popup__tabs-item[data-type="popup-tab-pickup-point"]');
     this._popupPickupPointBlockWithTab = document.querySelector('.popup__tabs-block[data-type="popup-tab-pickup-point-block"]');
     this._handleChangeElement = handleChangeElement;
+    this._tabsItems = [this._popupPickupTab, this._popupPickupPointTab];
+    this._tabsItemsBlocks = [this._popupPickupBlockWithTab, this._popupPickupPointBlockWithTab]
+  }
+
+  _setActiveTab = () => {
+    this._tabsItems.forEach(tab => tab.classList.remove('popup__tabs-item_active'));
+    this._tabsItemsBlocks.forEach(block => block.style.display = 'none');
+
+    if (this._activeTab) {
+      this._activeTab.classList.add('popup__tabs-item_active');
+      this._tabsItemsBlocks[this._tabsItems.indexOf(this._activeTab)].style.display = 'block';
+    } else {
+      this._tabsItems[this._tabsItems.length - 1].classList.add('popup__tabs-item_active');
+      this._tabsItemsBlocks[this._tabsItemsBlocks.length - 1].style.display = 'block';
+    }
   }
 
   setInitialAddress = () => {
-    this._activeTab = this._popupPickupPointTab;
-    this._popupPickupPointTab.classList.add('popup__tabs-item_active');
+    this._tabsItems[this._tabsItems.length - 1].classList.add('popup__tabs-item_active');
 
     this._pickupPointAddressList[this._pickupPointAddressList.length - 1].enableInput();
     this._handleChangeElement(this._pickupPointAddressList[this._pickupPointAddressList.length - 1]);
@@ -26,8 +40,6 @@ export default class PopupWithChooseAddress extends Popup {
     super.setEventListener();
 
     this._popupPickupTab.addEventListener('click', () => {
-      this._pickupAddressList.forEach(element => element.disableInput());
-      this._pickupAddressList[this._pickupPointAddressList.length - 1].enableInput();
 
       this._popupPickupPointBlockWithTab.style.display = 'none';
       this._popupPickupPointTab.classList.remove('popup__tabs-item_active');
@@ -36,8 +48,6 @@ export default class PopupWithChooseAddress extends Popup {
     })
 
     this._popupPickupPointTab.addEventListener('click', () => {
-      this._pickupPointAddressList.forEach(element => element.disableInput());
-      this._pickupPointAddressList[this._pickupPointAddressList.length - 1].enableInput();
 
       this._popupPickupBlockWithTab.style.display = 'none';
       this._popupPickupTab.classList.remove('popup__tabs-item_active');
@@ -46,10 +56,28 @@ export default class PopupWithChooseAddress extends Popup {
     })
 
     this._popupBtn.addEventListener('click', () => {
-      this._handleChangeElement(this._changeElement());
+      this._activeAddress = this._changeElement();
+      this._activeTab = this._tabsItems.find(tab => tab.classList.contains('popup__tabs-item_active'));
+
+      this._handleChangeElement(this._activeAddress);
 
       this.close();
     })
+  }
+
+  close() {
+    super.close();
+
+    this.disabledAllInputs();
+
+    if (this._activeAddress) this._activeAddress.enableInput();
+  }
+
+  open() {
+    super.open();
+
+    this._setActiveTab();
+    if (!this._activeAddress) this.setInitialAddress();
   }
 
   disabledAllInputs = () => {
